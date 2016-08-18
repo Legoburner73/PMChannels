4b88d413b2b0ae44971b926b4be242b1678575f4
Legoburner73 <legoplaysmcmail@gmail.com>
2016-08-18 21:31:08 +0100

    Create Main.php

---
+++ src/Lego/PMChannels/Main.php
@@ -0,0 +1,14 @@
+namespace Lego\PMChannels;
+
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\PlayerChatEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener {
+   public function onEnable() {
+   
+   }
+}
